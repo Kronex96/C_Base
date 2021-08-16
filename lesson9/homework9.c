1d3a32401f57047aa87d27271aa77a56292e3395
Kronex96 <Kronex96@gmail.com>
2021-08-17 00:51:23 +0300

    added checking function

--- lesson9/homework9.c
+++ lesson9/homework9.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 
 int str_sum_digits(const char *cs);
+int get_integers(const char *str);
+int summ_int(int a);
 
 int main(int argc, char const *argv[])
 {
 	char str1[100];
+	int a, b;
 
 	scanf("%s", str1);
 	printf("length of %s = %lu\n", str1, strlen(str1));
 	printf("%d\n",str_sum_digits(str1));
-		
+	
+	printf("\nchecking result\n");
+	a = get_integers(str1);
+	printf("all integers are %d\n", a);
+	b = summ_int(a);
+	printf("summ of them = %d\n", b);
+
 	return 0;
 }
 
@@ -32,4 +42,35 @@ int str_sum_digits(const char *cs)
 
 	return sum;
 
+}
+
+int get_integers(const char *str)
+{
+	int length = strlen(str);
+	char check[20];
+	int j = 0;
+	int a;
+	for (int i = 0; i < length; i++)
+	{
+		if ((str[i] >= '0') && (str[i] <= '9'))
+		{
+			check[j] = str[i];
+			j++;
+		}
+	}
+	
+	return atoi(check);
+}
+
+int summ_int(int a)
+{
+	int sum = 0;
+
+	while(a > 0)
+	{
+		sum = sum + (a % 10);
+		a = a / 10;
+	}
+
+	return sum;
 }
